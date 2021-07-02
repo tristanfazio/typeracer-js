2c2f77dfd06c73577603a13ff1403c93ca6206bf
tristanfazio <tristan.fazio@outlook.com.au>
2021-07-02 23:42:40 +0800

    update client with css modules and initial component

---
+++ client/src/components/GameMenu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import * as styles from './GameMenu.module.css'
+
+const GameMenu = () => {
+    return(
+        <div className = {styles.game}>
+            <div className = "gameList">
+        
+            </div>
+            <div className = "game">
+
+            </div>
+        </div>
+    );
+} 
+
+export default GameMenu
