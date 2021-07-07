40c6482800ee39fb7270fe90f5dc40de083dba80
tristanfazio <tristan.fazio@outlook.com.au>
2021-07-07 23:11:53 +0800

    add test websocket to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,26 @@
-import express from "express";
 import { Server, Socket } from "socket.io";
+import { Request, Response } from 'express';
 
-const app = express()
-const port = 3000
+const app = require("express")();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('This is the server!')
-})
-
-app.listen(port, () => {
+const httpServer = app.listen(port, () => {
   console.log(`Server starting on http://localhost:${port}`)
-})
+});
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('This is the server with websockets!')
+});
+
+io.on('connection', (socket: Socket) => {
+  console.log('Client connected');
+  socket.emit('test','this is a message from the server');
+});
