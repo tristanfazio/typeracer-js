aaa13f36a205e332e7707ce71aae398bcda6827b
tristanfazio <tristan.fazio@outlook.com.au>
2022-02-22 00:46:14 +0800

    Formatting

--- client/src/pages/GameMenu/GameMenu.tsx
+++ client/src/pages/GameMenu/GameMenu.tsx
@@ -1,62 +1,69 @@
-import { useEffect, useReducer } from 'react';
-import styles from './GameMenu.module.css'
-import { useHistory } from "react-router-dom";
+import { useEffect } from 'react';
+import styles from './GameMenu.module.css';
+import { useHistory } from 'react-router-dom';
 import { GameListEntry } from '../../types';
 import socket from '../../sockets/socketConfig';
-import lobbyListReducer, { initialState } from '../../state/lobbyList/lobbyListReducer';
 import { registerUpdateLobbyListener } from '../../sockets/lobbyListSockets';
-import { useLobbyListSelector } from '../../state/hooks';
+import { RootState } from '../../state/store';
+import { useSelector } from 'react-redux';
+
 interface ButtonProps {
     onClick: () => void;
 }
 
 interface GameRowProps {
-    gameName: string,
-    gameId: string
+    gameName: string;
+    gameId: string;
 }
 
 const GameMenu = () => {
-    const lobbyState = useLobbyListSelector((state) => state.lobbyList.gameList);
-    
+    const lobbyState = useSelector(
+        (state: RootState) => state.lobbyListState.gameList,
+    );
+
     let history = useHistory();
 
     useEffect(() => {
         registerUpdateLobbyListener();
         socket.emit('request-lobby-list');
-    }, [])
+    }, []);
 
     return (
-        <div className = {styles.gamesContainer}>
-            <GameListModal gameList = { lobbyState }/>
-            <div className = {styles.buttonContainer}>
-                <CreateButton onClick = {() => history.push('/game/create')}/>
+        <div className={styles.gamesContainer}>
+            <GameListModal gameList={lobbyState} />
+            <div className={styles.buttonContainer}>
+                <CreateButton onClick={() => history.push('/game/create')} />
             </div>
         </div>
     );
-}
+};
 
-const GameListModal = (props: {gameList: GameListEntry[]}) => {
+const GameListModal = (props: { gameList: GameListEntry[] }) => {
     return (
-        <div className = {styles.gameListModal}>
+        <div className={styles.gameListModal}>
             <ul>
-                { props.gameList.map((element: GameListEntry) => {
-                    return <GameListRow {...element} />
-                }) } 
+                {props.gameList.map((element: GameListEntry) => {
+                    return <GameListRow {...element} />;
+                })}
             </ul>
         </div>
     );
-}
+};
 
 const GameListRow = (props: GameRowProps) => {
     let history = useHistory();
 
     return (
-        <div className = {styles.gameListRow}>
-            <p>{props.gameName}</p>   <JoinButton onClick = {() => history.push({
-                pathname:`/game/join`,
-                state: {gameId: props.gameId}
-                })
-            }/>
+        <div className={styles.gameListRow}>
+            <p>{props.gameName}</p>
+            <JoinButton
+                onClick={() =>
+                    history.push({
+                        pathname: `/game/join`,
+                        state: { gameId: props.gameId },
+                    })
+                }
+            />
         </div>
     );
 };
@@ -64,25 +71,33 @@ const GameListRow = (props: GameRowProps) => {
 const CreateButton = (props: ButtonProps) => {
     return (
         <div>
-            <button type = "button" onClick = {props.onClick} className = {`
+            <button
+                type='button'
+                onClick={props.onClick}
+                className={`
                 ${styles.button} 
-                ${styles.createGameButton}`}>
-                    Create Game
+                ${styles.createGameButton}`}
+            >
+                Create Game
             </button>
         </div>
     );
-}
+};
 
 const JoinButton = (props: ButtonProps) => {
-    return(
+    return (
         <div>
-            <button type = "button" onClick = {props.onClick} className = {`
+            <button
+                type='button'
+                onClick={props.onClick}
+                className={`
                 ${styles.joinGameButton} 
-                ${styles.button}`}>
-                    Join Game
+                ${styles.button}`}
+            >
+                Join Game
             </button>
         </div>
     );
-}
+};
 
 export default GameMenu;
