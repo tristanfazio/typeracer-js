f8b1fbd966ef8252818518b88dac3eae5fcc9d3c
tristanfazio <tristan.fazio@outlook.com.au>
2022-03-10 23:23:49 +0800

    move quote to own component

---
+++ client/src/components/Quote/Quote.tsx
@@ -0,0 +1,58 @@
+import { useSelector } from 'react-redux';
+import { FillState } from '../../state/gameState/gameStateReducer';
+import { RootState } from '../../state/store';
+import styles from './Quote.module.css';
+
+export function Quote({ quoteArray }) {
+    const currentWordIndex = useSelector(
+        (state: RootState) => state.gameState.currentWordIndex,
+    );
+
+    const currentLetterIndex = useSelector(
+        (state: RootState) => state.gameState.currentLetterIndex,
+    );
+
+    const mappedComponents = quoteArray.map((word, wordIndex) => {
+        return (
+            <span
+                key={wordIndex}
+                className={wordIndex < currentWordIndex ? styles.correct : ''}
+            >
+                {word.map((letter, letterIndex) => {
+                    const key = `${wordIndex}-${letterIndex}`;
+                    const cursor =
+                        wordIndex === currentWordIndex &&
+                        letterIndex === currentLetterIndex
+                            ? styles.cursor
+                            : '';
+                    const correct =
+                        letter.fillState === FillState.CORRECT
+                            ? styles.correct
+                            : '';
+                    const error =
+                        letter.fillState === FillState.ERROR
+                            ? letter.character === ' '
+                                ? styles.errorSpace
+                                : styles.error
+                            : '';
+                    const style = error + correct + cursor;
+                    const element =
+                        letter.character === ' ' ? (
+                            <span key={key} className={style}>
+                                &nbsp;
+                            </span>
+                        ) : (
+                            <span key={key} className={style}>
+                                {letter.character}
+                            </span>
+                        );
+                    return element;
+                })}
+            </span>
+        );
+    });
+
+    return <>{mappedComponents}</>;
+}
+
+export default Quote;
--- client/src/components/QuoteContainer/QuoteContainer.tsx
+++ client/src/components/QuoteContainer/QuoteContainer.tsx
@@ -1,8 +1,5 @@
-import {
-    FillState,
-    GameState,
-    GameStatus,
-} from '../../state/gameState/gameStateReducer';
+import { Quote } from './../Quote/Quote';
+import { GameState, GameStatus } from '../../state/gameState/gameStateReducer';
 import Countdown from '../Countdown';
 import styles from './QuoteContainer.module.css';
 
@@ -11,64 +8,23 @@ const QuoteContainer = (props: { gameState: GameState }) => {
     const gameState = props.gameState;
     const quoteArray = gameState.quoteArray;
     const status = gameState.status;
-    const mappedComponents = quoteArray.map((word, wordIndex) => {
-        return (
-            <span
-                key={wordIndex}
-                className={
-                    wordIndex < gameState.currentWordIndex ? styles.correct : ''
-                }
-            >
-                {word.map((letter, letterIndex) => {
-                    const key = `${wordIndex}-${letterIndex}`;
-                    const cursor =
-                        wordIndex === gameState.currentWordIndex &&
-                        letterIndex === gameState.currentLetterIndex
-                            ? styles.cursor
-                            : '';
 
-                    const correct =
-                        letter.fillState === FillState.CORRECT
-                            ? styles.correct
-                            : '';
-
-                    const error =
-                        letter.fillState === FillState.ERROR
-                            ? letter.character === ' '
-                                ? styles.errorSpace
-                                : styles.error
-                            : '';
-
-                    const style = error + correct + cursor;
-                    const element =
-                        letter.character === ' ' ? (
-                            <span key={key} className={style}>
-                                &nbsp;
-                            </span>
-                        ) : (
-                            <span key={key} className={style}>
-                                {letter.character}
-                            </span>
-                        );
-                    return element;
-                })}
-            </span>
-        );
-    });
-
-    const finishedQuote = gameState.completedWordCount === gameState.quoteArray.length;
+    const finishedQuote =
+        gameState.completedWordCount === gameState.quoteArray.length;
     return (
         <div className={styles.quoteContainer}>
-            {mappedComponents}
+            <Quote quoteArray={quoteArray} />
             {status === GameStatus.COUNTDOWN && <Countdown />}
-            {status === GameStatus.FINISHED && <Finish finishedQuote = { finishedQuote } />}
+            {status === GameStatus.FINISHED && (
+                <Finish finishedQuote={finishedQuote} />
+            )}
         </div>
     );
 };
 
-const Finish = ({finishedQuote}) => {
+const Finish = ({ finishedQuote }) => {
     const finishText = finishedQuote ? <p>Finished!</p> : <p>Times Up!</p>;
-    return (<div className={styles.finish}>{finishText}</div>);
+    return <div className={styles.finish}>{finishText}</div>;
 };
 
 export default QuoteContainer;
